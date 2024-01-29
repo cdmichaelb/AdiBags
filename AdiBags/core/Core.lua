0c2bab85c86fa0f88827d6ae297db2e3922cc0a4
Szyler <Szyler@Szyler.com>
2024-01-29 20:52:12 +0100

    Adding aceevent for Outfitter mod

--- AdiBags/core/Core.lua
+++ AdiBags/core/Core.lua
@@ -44,7 +44,7 @@ local type = _G.type
 local unpack = _G.unpack
 --GLOBALS>
 
-local addon = LibStub('AceAddon-3.0'):NewAddon(ns, addonName, 'ABEvent-1.0', 'ABBucket-1.0', 'AceHook-3.0', 'AceConsole-3.0', 'LibCompat-1.0')--, 'LibItemSearch-1.2-ElvUI')
+local addon = LibStub('AceAddon-3.0'):NewAddon(ns, addonName, 'ABEvent-1.0', 'ABBucket-1.0', 'AceHook-3.0', 'AceConsole-3.0', 'LibCompat-1.0', 'AceEvent-3.0')--, 'LibItemSearch-1.2-ElvUI')
 addon.itemSearch = LibStub("LibItemSearch-1.2-ElvUI")
 --local LC = LibStub("LibCompat-1.0")
 --LC:Embed(addon)
